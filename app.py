0ff1a1ae4695be5a05001333b59298388d3d9403
Kurk3 <kurekadam314@gmail.com>
2022-04-05 21:03:30 +0200

    commit pridanie blogov a routov.

--- app.py
+++ app.py
@@ -17,17 +17,179 @@ def contact_me():
     return render_template('contact_me.html')
 
 
+
+
 ###################################################################################
 # kategorie #######################################################################
+@app.route('/productivity')
+def productivity_main():
+    return render_template('blog_categories/Productivity/choose_blog_productivity.html')
 
-@app.route('/health_fitness')
-def health_fitness():
+
+@app.route('/productivity/blog_1')
+def productivity_1():
+    return render_template('specific_blogs/Productivity/blog_productivity_1.html')
+
+@app.route('/productivity/blog_2')
+def productivity_2():
+    return render_template('specific_blogs/Productivity/blog_productivity_2.html')
+
+@app.route('/productivity/blog_3')
+def productivity_3():
+    return render_template('specific_blogs/Productivity/blog_productivity_3.html')
+
+@app.route('/productivity/blog_4')
+def productivity_4():
+    return render_template('specific_blogs/Productivity/blog_productivity_4.html')
+
+@app.route('/productivity/blog_5')
+def productivity_5():
+    return render_template('specific_blogs/Productivity/blog_productivity_5.html')
+
+
+@app.route('/web_development')
+def web_dev_main():
+    return render_template('blog_categories/Web/choose_blog_webdev.html')
+
+@app.route('/web_development/blog_1')
+def web_development_1():
+    return render_template('specific_blogs/WebDevelopment/blog_webdev_1.html')
+
+@app.route('/web_development/blog_2')
+def web_development_2():
+    return render_template('specific_blogs/WebDevelopment/blog_webdev_2.html')
+
+@app.route('/web_development/blog_3')
+def web_development_3():
+    return render_template('specific_blogs/WebDevelopment/blog_webdev_3.html')
+
+@app.route('/web_development/blog_4')
+def web_development_4():
+    return render_template('specific_blogs/WebDevelopment/blog_webdev_4.html')
+
+@app.route('/web_development/blog_5')
+def web_development_5():
+    return render_template('specific_blogs/WebDevelopment/blog_webdev_5.html')
+
+
+
+
+
+
+
+@app.route('/hobby')
+def hobby_main():
+    return render_template('blog_categories/Hobbies/choose_blog_hobbies.html')
+
+@app.route('/hobby/blog_1')
+def hobby_1():
+    return render_template('specific_blogs/Hobbies/blog_hobby_1.html')
+
+@app.route('/hobby/blog_2')
+def hobby_2():
+    return render_template('specific_blogs/Hobbies/blog_hobby_2.html')
+
+@app.route('/hobby/blog_3')
+def hobby_3():
+    return render_template('specific_blogs/Hobbies/blog_hobby_3.html')
+
+@app.route('/hobby/blog_4')
+def hobby_4():
+    return render_template('specific_blogs/Hobbies/blog_hobby_4.html')
+
+@app.route('/hobby/blog_5')
+def hobby_5():
+    return render_template('specific_blogs/Hobbies/blog_hobby_5.html')
+
+@app.route('/finances')
+def finances_main():
+    return render_template('blog_categories/Finances/choose_blog_finances.html')
+
+@app.route('/finances/blog_1')
+def finances_1():
+    return render_template('specific_blogs/Finances/blog_finances_1.html')
+
+@app.route('/finances/blog_2')
+def finances_2():
+    return render_template('specific_blogs/Finances/blog_finances_2.html')
+
+@app.route('/finances/blog_3')
+def finances_3():
+    return render_template('specific_blogs/Finances/blog_finances_3.html')
+
+@app.route('/finances/blog_4')
+def finances_4():
+    return render_template('specific_blogs/Finances/blog_finances_4.html')
+
+@app.route('/finances/blog_5')
+def finances_5():
+    return render_template('specific_blogs/Finances/blog_finances_5.html')
+
+
+
+@app.route('/learning')
+def learning_main():
+    return render_template('blog_categories/Learning/choose_blog_learning.html')
+
+@app.route('/learning/blog_1')
+def learning_1():
+    return render_template('specific_blogs/Learning/blog_learning_1.html')
+
+@app.route('/learning/blog_2')
+def learning_2():
+    return render_template('specific_blogs/Learning/blog_learning_1.html')
+
+@app.route('/learning/blog_3')
+def learning_3():
+    return render_template('specific_blogs/Learning/blog_learning_3.html')
+
+@app.route('/learning/blog_4')
+def learning_4():
+    return render_template('specific_blogs/Learning/blog_learning_4.html')
+
+@app.route('/learning/blog_5')
+def learning_5():
+    return render_template('specific_blogs/Learning/blog_learning_5.html')
+
+
+
+
+
+
+
+@app.route('/fitness')
+def fitness_main():
     return render_template('blog_categories/Health/choose_blog_health.html')
 
+@app.route('/fitness/blog_1')
+def fitness_1():
+    return render_template('specific_blogs/HealthFitness/blog_fitness_1.html')
 
-@app.route('/health_fitness/blog_health_1')
-def blog_health_1():
-    return render_template('specific_blogs/HealthFitness/blog_health_brain.html')
+@app.route('/fitness/blog_2')
+def fitness_2():
+    return render_template('specific_blogs/HealthFitness/blog_fitness_2.html')
+
+@app.route('/fitness/blog_3')
+def fitness_3():
+    return render_template('specific_blogs/HealthFitness/blog_fitness_3.html')
+
+@app.route('/fitness/blog_4')
+def fitness_4():
+    return render_template('specific_blogs/HealthFitness/blog_fitness_4.html')
+
+@app.route('/fitness/blog_5')
+def fitness_5():
+    return render_template('specific_blogs/HealthFitness/blog_fitness_5.html')
+
+
+
+
+
+
+
+
+
+
 
 
 ###################################################################################
